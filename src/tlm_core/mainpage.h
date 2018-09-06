d1cec9d5c59bc66d9ba9c12518143463fac6f653
Phil Mulholland <pmulholland@shincbm.com>
2018-09-06 22:45:01 +0930

    Add treeview, search and top description.

---
+++ src/tlm_core/mainpage.h
@@ -0,0 +1,34 @@
+/*! \mainpage SystemC TLM
+ *
+ * \section source_sec Original Source
+ *
+ * This reference implementation is sourced from http://www.accellera.org/downloads/standards/systemc.
+ *
+ *
+ * \section intro_sec Introduction
+ *
+ * http://www.accellera.org/community/systemc/about-systemc-tlm
+ *
+* About SystemC TLM
+* 
+* Transaction-level Modeling (TLM) standard interfaces for SystemC
+* provides an essential framework needed for model exchange within
+* companies and across the IP supply chain for architecture analysis,
+* software development and performance analysis, and hardware
+* verification. It explicitly addresses virtual prototyping in which
+* SystemC models can easily be exchanged and arranged within a
+* system. By providing a strong modeling foundation for virtual
+* prototyping, the standard enables optimal reuse of models and modeling
+* effort across different use cases.
+* 
+* 
+ * \section license_sec License
+ *
+* In 2016 Accellera re-licensed all SystemC supplemental material under
+* the Apache 2.0 License. Access to some previous releases of SystemC
+* material may still require acceptance of the terms of the SystemC Open
+* Source License while the process of updating license statements is
+* underway. Please contact the Accellera office if you have any
+* questions.
+* 
+ */
