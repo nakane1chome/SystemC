e28b374466e3c2da691784df3157cb4ecb752adb
Phil Mulholland <pmulholland@shincbm.com>
2018-09-06 22:24:49 +0930

    Top description and attribution

---
+++ src/sysc/mainpage.h
@@ -0,0 +1,36 @@
+/*! \mainpage SystemC 
+ *
+ * \section source_sec Original Source
+ *
+ * This reference implementation is sourced from http://www.accellera.org/downloads/standards/systemc.
+ *
+ *
+ * \section intro_sec Introduction
+ *
+ * http://www.accellera.org/community/systemc/about-systemc
+ *
+* SystemC® addresses the need for a system design and verification
+* language that spans hardware and software. It is a language built in
+* standard C++ by extending the language with the use of class
+* libraries. The language is particularly suited to model system's
+* partitioning, to evaluate and verify the assignment of blocks to
+* either hardware or software implementations, and to architect and
+* measure the interactions between and among functional blocks. Leading
+* companies in the intellectual property (IP), electronic design
+* automation (EDA), semiconductor, electronic systems, and embedded
+* software industries currently use SystemC for architectural
+* exploration, to deliver high-performance hardware blocks at various
+* levels of abstraction and to develop virtual platforms for
+* hardware/software co-design. SystemC was defined by the Open SystemC
+* Initiative (OSCI) and ratified as IEEE Std. 1666™-2011.
+* 
+ * \section license_sec License
+ *
+* In 2016 Accellera re-licensed all SystemC supplemental material under
+* the Apache 2.0 License. Access to some previous releases of SystemC
+* material may still require acceptance of the terms of the SystemC Open
+* Source License while the process of updating license statements is
+* underway. Please contact the Accellera office if you have any
+* questions.
+* 
+ */
